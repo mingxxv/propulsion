e97e081efbd14d6b7946a2676d72d0209e4624e6
mingxxv <mingxxv@protonmail.ch>
2021-07-01 16:24:51 +0800

    what if i uploaded the damn thing anyway lol

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import Inspiration from './Inspiration.js'
 
 function App() {
   return (
@@ -17,7 +19,9 @@ function App() {
         >
           Learn React
         </a>
+
       </header>
+      <Inspiration />
     </div>
   );
 }
