93254cd875c6e878509c604d670821f6114562c7
mingxxv <mingxxv@protonmail.ch>
2021-07-01 16:47:42 +0800

    help lah

--- src/Inspiration.js
+++ src/Inspiration.js
@@ -63,23 +63,3 @@ useEffect(() => {
 
 
 
-
-// fetch("https://inspiration.goprogram.ai/", {
-//   "headers": {
-//     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-//     "accept-language": "en-SG,en-US;q=0.9,en;q=0.8",
-//     "cache-control": "max-age=0",
-//     "sec-ch-ua": "\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"",
-//     "sec-ch-ua-mobile": "?0",
-//     "sec-fetch-dest": "document",
-//     "sec-fetch-mode": "navigate",
-//     "sec-fetch-site": "none",
-//     "sec-fetch-user": "?1",
-//     "upgrade-insecure-requests": "1"
-//   },
-//   "referrerPolicy": "strict-origin-when-cross-origin",
-//   "body": null,
-//   "method": "GET",
-//   "mode": "cors",
-//   "credentials": "omit"
-// });
